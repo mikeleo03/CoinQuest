8877834d3ba70bfa82a259c2d0976109ab3a15ec
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-09-24 18:13:45 +0700

    feat : navbar basic

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import Navbar from "@/components/ui/navbar-login";
+import Navbar from "@/components/ui/navbar-basic";
 
 const homePage = () => {
   return (
---
+++ components/ui/navbar-basic.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const navbar = () => {
+    return (
+        <div>
+            <nav className="bg-title fixed z-50 bg-clip-padding backdrop-filter backdrop-blur-md" style={{width: "100vw"}}>
+                <div className="w-full mx-auto px-12">
+                    <div className="flex items-center justify-between h-24">
+                        <div className="flex-shrink-0">
+                            <a href="/">
+                                <img className="h-12 w-auto" src="/assets/logo.png" alt="logo" />
+                            </a>
+                        </div>
+                        <div className="items-center">
+                            <div className="ml-10 flex items-center space-x-7">
+                                <a
+                                    href="/login"
+                                    className=" hover:scale-110 text-white border-white border-2 px-5 py-2 rounded-3xl text-[20px] font-medium font-poppins items-center transition-transform duration-300"
+                                >
+                                    Log In
+                                </a>
+                                <a
+                                    href="/signup"
+                                    className=" hover:scale-110 bg-black text-white px-5 py-2 rounded-3xl text-[20px] font-medium font-poppins items-center transition-transform duration-300"
+                                >
+                                    Sign Up
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default navbar;
