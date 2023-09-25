577f507994cf491c1446e78b51e7b2c5d30c3dab
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-09-25 12:36:58 +0700

    feat: tasks endpoint

---
+++ app/api/(tasks)/all-task/route.ts
@@ -0,0 +1,17 @@
+
+import { NextRequest, NextResponse } from "next/server";
+import supabase from "@/utils/supabase";
+
+export async function GET(req: NextRequest) {
+
+  const { data, error } = await supabase
+    .from('Tasks')
+    .select('*');
+
+    if (error) {
+      console.error(error); // Log the error for debugging purposes
+      return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+    }
+
+  NextResponse.json({ data }, { status: 400 });
+}
