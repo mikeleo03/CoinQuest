ec82f09997320dcf7fa6aed6bde7ee407c2b21f9
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-09-25 12:08:51 +0700

    fix: get all courses endpoint

--- app/api/(course)/all-course/route.ts
+++ app/api/(course)/all-course/route.ts
@@ -1,10 +1,8 @@
-import { URLSearchParams } from 'next/dist/compiled/@edge-runtime/primitives/url';
+
 import { NextRequest, NextResponse } from "next/server";
 import supabase from "@/utils/supabase";
 
 export async function GET(req: NextRequest) {
-  // const { searchParams } = new URL(req.url);
-  // const title = searchParams.get('title');
 
   const { data, error } = await supabase
     .from('Courses')
