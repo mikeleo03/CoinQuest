ad4f3d0d22981382a0dec732ec345b9500d2c8ee
Salomo309 <109785084+Salomo309@users.noreply.github.com>
2023-09-25 12:08:40 +0700

    feat: get single course endpoint

---
+++ app/api/(course)/course/[id]/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import supabase from "@/utils/supabase";
+
+export async function GET(req: NextRequest) {
+    const queryParams = new URLSearchParams(req.url.split("?")[1]);
+    const courseId = queryParams.get("id");
+
+  try {
+    const { data: course, error } = await supabase
+      .from('Courses')
+      .select('*')
+      .eq('id', courseId)
+      .single(); // Mengambil satu baris kursus berdasarkan ID
+
+    if (error) {
+      console.error(error); // Log the error for debugging purposes
+      return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+    }
+
+    if (!course) {
+      return NextResponse.json({ error: "Course not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ data: course }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}
