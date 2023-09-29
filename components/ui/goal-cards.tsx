2aef8a387474c43607175dc89bec377916475fea
Michael Leon Putra Widhi <leonmichael463@gmail.com>
2023-09-29 17:28:47 +0700

    feat : update goal cards

--- components/ui/goal-cards.tsx
+++ components/ui/goal-cards.tsx
@@ -10,11 +10,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
 
 type Props = {
   id: number;
@@ -100,28 +95,29 @@ const GoalCard: React.FC<CardProps> = ({ id, title, desc, price, is_done, onClic
               draggable='false'
           />
         </div>
-        <Popover>
-          <PopoverTrigger>
-            <button className="transition-transform duration-200 transform hover:scale-125 fixed right-72 top-[7.5rem] justify-center items-center">
+        <Dialog>
+          <DialogTrigger>
+            <div className="transition-transform duration-200 transform hover:scale-125 fixed right-72 top-[7.5rem] justify-center items-center">
               <img
                 src="/assets/planets/planet-fix-1.png"
                 alt="background image"
                 className="w-[80px] h-[80px]"
                 draggable='false'
               />
-            </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
+            </div>
+          </DialogTrigger>
+          <DialogContent className="w-80">
             <div className="grid gap-4">
               <h4 className="font-medium leading-none">Task 1</h4>
               <p className="text-sm text-muted-foreground mb-2">
                 Berikut adalah beberapa task yang perlu diselesaikan :
               </p>
+              <button onClick={() => console.log("Hai")} className="z-[400]">Hai dunia</button>
             </div>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger>
             <button className="transition-transform duration-200 transform hover:scale-125 fixed right-[12rem] bottom-[18rem] justify-center items-center">
               <img
                 src="/assets/planets/planet-fix-2.png"
@@ -130,18 +126,18 @@ const GoalCard: React.FC<CardProps> = ({ id, title, desc, price, is_done, onClic
                 draggable='false'
               />
             </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
+          </DialogTrigger>
+          <DialogContent className="w-80">
             <div className="grid gap-4">
               <h4 className="font-medium leading-none">Task 2</h4>
               <p className="text-sm text-muted-foreground mb-2">
                 Berikut adalah beberapa task yang perlu diselesaikan :
               </p>
             </div>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger>
             <button className="transition-transform duration-200 transform hover:scale-125 fixed right-[22rem] bottom-[6rem] justify-center items-center">
               <img
                 src="/assets/planets/planet-fix-3.png"
@@ -150,18 +146,18 @@ const GoalCard: React.FC<CardProps> = ({ id, title, desc, price, is_done, onClic
                 draggable='false'
               />
             </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
+          </DialogTrigger>
+          <DialogContent className="w-80">
             <div className="grid gap-4">
               <h4 className="font-medium leading-none">Task 3</h4>
               <p className="text-sm text-muted-foreground mb-2">
                 Berikut adalah beberapa task yang perlu diselesaikan :
               </p>
             </div>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger>
             <button className="transition-transform duration-200 transform hover:scale-125 fixed left-[26.2rem] bottom-[13rem] justify-center items-center">
               <img
                 src="/assets/planets/planet-fix-4.png"
@@ -170,18 +166,18 @@ const GoalCard: React.FC<CardProps> = ({ id, title, desc, price, is_done, onClic
                 draggable='false'
               />
             </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
+          </DialogTrigger>
+          <DialogContent className="w-80">
             <div className="grid gap-4">
               <h4 className="font-medium leading-none">Task 4</h4>
               <p className="text-sm text-muted-foreground mb-2">
                 Berikut adalah beberapa task yang perlu diselesaikan :
               </p>
             </div>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger>
             <button className="transition-transform duration-200 transform hover:scale-125 fixed right-[27rem] top-[15.5rem] justify-center items-center">
               <img
                 src="/assets/planets/planet-fix-5.png"
@@ -190,18 +186,18 @@ const GoalCard: React.FC<CardProps> = ({ id, title, desc, price, is_done, onClic
                 draggable='false'
               />
             </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
+          </DialogTrigger>
+          <DialogContent className="w-80">
             <div className="grid gap-4">
               <h4 className="font-medium leading-none">Task 5</h4>
               <p className="text-sm text-muted-foreground mb-2">
                 Berikut adalah beberapa task yang perlu diselesaikan :
               </p>
             </div>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger>
             <button className="transition-transform duration-200 transform hover:scale-125 fixed left-[17.6rem] top-[10.4rem] justify-center items-center">
               <img
                 src="/assets/planets/planet-fix-6.png"
@@ -210,18 +206,18 @@ const GoalCard: React.FC<CardProps> = ({ id, title, desc, price, is_done, onClic
                 draggable='false'
               />
             </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
+          </DialogTrigger>
+          <DialogContent className="w-80">
             <div className="grid gap-4">
               <h4 className="font-medium leading-none">Task 6</h4>
               <p className="text-sm text-muted-foreground mb-2">
                 Berikut adalah beberapa task yang perlu diselesaikan :
               </p>
             </div>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger>
             <button className="transition-transform duration-200 transform hover:scale-125 fixed left-[12.7rem] bottom-[15.5rem] justify-center items-center">
               <img
                 src="/assets/planets/planet-fix-7.png"
@@ -230,18 +226,18 @@ const GoalCard: React.FC<CardProps> = ({ id, title, desc, price, is_done, onClic
                 draggable='false'
               />
             </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
+          </DialogTrigger>
+          <DialogContent className="w-80">
             <div className="grid gap-4">
               <h4 className="font-medium leading-none">Task 7</h4>
               <p className="text-sm text-muted-foreground mb-2">
                 Berikut adalah beberapa task yang perlu diselesaikan :
               </p>
             </div>
-          </PopoverContent>
-        </Popover>
-        <Popover>
-          <PopoverTrigger>
+          </DialogContent>
+        </Dialog>
+        <Dialog>
+          <DialogTrigger>
             <button className="transition-transform duration-200 transform hover:scale-125 fixed left-[17.8rem] bottom-[7rem] justify-center items-center">
               <img
                 src="/assets/planets/planet-fix-8.png"
@@ -250,16 +246,16 @@ const GoalCard: React.FC<CardProps> = ({ id, title, desc, price, is_done, onClic
                 draggable='false'
               />
             </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-80">
+          </DialogTrigger>
+          <DialogContent className="w-80">
             <div className="grid gap-4">
               <h4 className="font-medium leading-none">Task 8</h4>
               <p className="text-sm text-muted-foreground mb-2">
                 Berikut adalah beberapa task yang perlu diselesaikan :
               </p>
             </div>
-          </PopoverContent>
-        </Popover>
+          </DialogContent>
+        </Dialog>
       </DialogContent>
     </Dialog>
   );
